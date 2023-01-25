0f9388aa05694ea6b371c8d80a701c0d805819e2
DiegoAlejandroTobonHenao <60477478+DiegoAlejandroTobonHenao@users.noreply.github.com>
2023-01-25 14:19:25 -0500

    Add files via upload

---
+++ servicio-catalogo/servicio-catalogo/src/test/java/com/libreriapolar/serviciocatalogo/ServicioCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libreriapolar.serviciocatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
